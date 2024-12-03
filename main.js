c5caa8595a1b257abad21a515ee87982bbea395a
Henry Zeng <youhengzeng8@gmail.com>
2024-12-03 12:24:30 -0500

    add function to display weather condition

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const API_KEY = 'ab4599317c716661f0d19f450d91f994'
+
+function displayWeather(data) {
+  document.getElementById('weather-info').innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <div class="weather-details">
+            <p>Temperature: ${Math.round(data.main.temp)}°C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+        </div>
+    `
+}
